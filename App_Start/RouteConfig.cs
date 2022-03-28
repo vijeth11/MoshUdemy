4e0d6bca2fe84648250c54f7d4d4dc3eda14b883
Vijeth Belle <vijeth11@yahoo.co.in>
2022-03-28 10:48:13 +0530

    project commit

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Vidly
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            // new routing approch now we can define route attribute in controller
+            routes.MapMvcAttributeRoutes();
+
+            /*old approch of creating routes and these needs to be arranged from specific to generic
+            if default is added above this route definition it will never be called
+
+            routes.MapRoute(
+                "MoviesByReleaseDate",
+                "movies/released/{year}/{month}",
+                new { controller = "Movie", action = "ByReleaseDate"},
+                new { year = @"\d{4}", month = @"\d{2}"});*/
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Controllers/MovieController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name="shrek"};
+            var customers = new List<Customer> {
+                new Customer{ Name = "Customer 1"},
+                new Customer{ Name = "Customer 2"}
+            };
+            var randomMovieModel = new RandomMovieViewModel { movie = movie, customers = customers};
+            return View(randomMovieModel);
+            /* different returns */
+            //return ContentResult("hello");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }
+
+        public ActionResult Edit(int movieId)
+        {
+            // URLS when parameter name is id:
+            // movie/edit/1
+            // movie/edit?id=1
+            // URLS when parameter name is movieId:
+            // movie/edit?movieId=1
+            return Content("id=" + movieId);
+        }
+
+        public ActionResult Index(int? pageNo, string sortBy)
+        {
+            // URLS
+            // movie
+            // movie?pageNo=1
+            // movie?pageNo=1&sortBy=text
+            if (!pageNo.HasValue)
+                pageNo = 1;
+            if (String.IsNullOrEmpty(sortBy))
+                sortBy = "Name";
+            return Content(String.Format("page number={0} and sort by = {1}",pageNo,sortBy));
+        }
+
+        [Route("movie/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(String.Format("{0}/{1}",year, month));
+        }
+    }
+}
